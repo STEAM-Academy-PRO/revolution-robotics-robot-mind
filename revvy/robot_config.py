8d67772eef04215251624377881067b5005542a4
Dániel Buga <daniel@revolutionrobotics.org>
2019-11-12 10:33:14 +0100

    More information in exceptions

--- revvy/robot_config.py
+++ revvy/robot_config.py
@@ -60,20 +60,28 @@ class RobotConfig:
             print(traceback.format_exc())
             return None
 
+        config = RobotConfig()
         try:
-            config = RobotConfig()
-
             robot_config = dict_get_first(json_config, ['robotConfig', 'robotconfig'])
             blockly_list = dict_get_first(json_config, ['blocklyList', 'blocklylist'])
+        except KeyError:
+            print('Received configuration is missing required parts')
+            print(traceback.format_exc())
+            return None
 
+        try:
             i = 0
             for script in blockly_list:
                 try:
-                    script_name = dict_get_first(script, ['builtinScriptName', 'builtinscriptname'])
-                    runnable = builtin_scripts[script_name]
+                    try:
+                        script_name = dict_get_first(script, ['builtinScriptName', 'builtinscriptname'])
+                        runnable = builtin_scripts[script_name]
+                    except KeyError:
+                        source_b64_encoded = dict_get_first(script, ['pythonCode', 'pythoncode'])
+                        runnable = b64_decode_str(source_b64_encoded)
                 except KeyError:
-                    source_b64_encoded = dict_get_first(script, ['pythonCode', 'pythoncode'])
-                    runnable = b64_decode_str(source_b64_encoded)
+                    print('Neither builtinScriptName, nor pythonCode is present for a script')
+                    raise
 
                 assignments = script['assignments']
                 if 'analog' in assignments:
@@ -101,49 +109,58 @@ class RobotConfig:
                     config.scripts[script_name] = {'script': runnable, 'priority': priority}
                     config.background_scripts.append(script_name)
                     i += 1
+        except (TypeError, IndexError, KeyError, ValueError):
+            print('Failed to decode received controller configuration')
+            print(traceback.format_exc())
+            return None
 
-            if 'motors' in robot_config:
-                i = 1
-                for motor in robot_config['motors']:
-                    if not motor:
-                        motor = {'type': 0}
+        try:
+            i = 1
+            motors = robot_config.get('motors', []) if type(robot_config) is dict else []
+            for motor in motors:
+                if not motor:
+                    motor = {'type': 0}
 
-                    if motor['type'] == 0:
-                        motor_type = motor_types[motor['type']]
+                if motor['type'] == 0:
+                    motor_type = motor_types[motor['type']]
 
-                    elif motor['type'] == 1:
-                        # motor
-                        motor_type = motor_types[1]
-                        config.motors.names[motor['name']] = i
+                elif motor['type'] == 1:
+                    # motor
+                    motor_type = motor_types[1]
+                    config.motors.names[motor['name']] = i
 
-                    elif motor['type'] == 2:
-                        # drivetrain
-                        motor_type = motor_types[2][motor['side']][motor['reversed']]
-                        config.motors.names[motor['name']] = i
-                        config.drivetrain[motor_sides[motor['side']]].append(i)
+                elif motor['type'] == 2:
+                    # drivetrain
+                    motor_type = motor_types[2][motor['side']][motor['reversed']]
+                    config.motors.names[motor['name']] = i
+                    config.drivetrain[motor_sides[motor['side']]].append(i)
 
-                    else:
-                        raise ValueError
+                else:
+                    raise ValueError('Unknown motor type: {}'.format(motor['type']))
 
-                    config.motors[i] = motor_type
+                config.motors[i] = motor_type
+                i += 1
+        except (TypeError, IndexError, KeyError, ValueError):
+            print('Failed to decode received motor configuration')
+            print(traceback.format_exc())
+            return None
 
-                    i += 1
+        try:
+            i = 1
+            sensors = robot_config.get('sensors', []) if type(robot_config) is dict else []
+            for sensor in sensors:
+                if not sensor or sensor['type'] == 0:
+                    sensor_type = "NotConfigured"
+                else:
+                    sensor_type = sensor_types[sensor['type']]
+                    config.sensors.names[sensor['name']] = i
+                config.sensors[i] = sensor_type
 
-            if 'sensors' in robot_config:
-                i = 1
-                for sensor in robot_config['sensors']:
-                    if not sensor or sensor['type'] == 0:
-                        sensor_type = "NotConfigured"
-                    else:
-                        sensor_type = sensor_types[sensor['type']]
-                        config.sensors.names[sensor['name']] = i
-                    config.sensors[i] = sensor_type
-
-                    i += 1
+                i += 1
 
             return config
         except (TypeError, IndexError, KeyError, ValueError):
-            print('Failed to decode received configuration')
+            print('Failed to decode received sensor configuration')
             print(traceback.format_exc())
             return None
 
