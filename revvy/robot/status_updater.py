98c6946ed6a1a93329860c6738a4a1836c07abdc
Dániel Buga <daniel@revolutionrobotics.org>
2019-12-12 19:34:53 +0100

    Use slot names instead of raw numbers

--- revvy/robot/status_updater.py
+++ revvy/robot/status_updater.py
@@ -3,17 +3,25 @@
 from revvy.mcu.rrrc_control import RevvyControl
 from revvy.utils.logger import Logger
 
-mcu_updater_slots = {
-    "motors": {i: i-1 for i in range(1, 7)},
-    "sensors": {i: i-1+6 for i in range(1, 5)},
-    "battery": 10,
-    "axl": 11,
-    "gyro": 12,
-    "yaw": 13
-}
-
 
 class McuStatusUpdater:
+    mcu_updater_slots = {
+        "motor_1": 0,
+        "motor_2": 1,
+        "motor_3": 2,
+        "motor_4": 3,
+        "motor_5": 4,
+        "motor_6": 5,
+        "sensor_1": 6,
+        "sensor_2": 7,
+        "sensor_3": 8,
+        "sensor_4": 9,
+        "battery": 10,
+        "axl": 11,
+        "gyro": 12,
+        "yaw": 13,
+        "reset": 14
+    }
     """Class to read status from the MCU
 
     This class is the counterpart of McuStatusUpdater/McuStatusUpdaterWrapper implemented on the MCU and is used
@@ -22,7 +30,7 @@ class McuStatusUpdater:
     def __init__(self, robot: RevvyControl):
         self._robot = robot
         self._is_enabled = [False] * 32
-        self._is_enabled[14] = True
+        self._is_enabled[self.mcu_updater_slots["reset"]] = True
         self._handlers = [lambda x: None] * 32
         self._log = Logger('McuStatusUpdater')
 
@@ -30,7 +38,7 @@ class McuStatusUpdater:
         self._log('reset all slots')
         self._handlers = [lambda x: None] * 32
         self._is_enabled = [False] * 32
-        self._is_enabled[14] = True
+        self._is_enabled[self.mcu_updater_slots["reset"]] = True
         self._robot.status_updater_reset()
 
     def _enable_slot(self, slot):
@@ -41,19 +49,18 @@ class McuStatusUpdater:
         self._log('disable slot {}'.format(slot))
         self._robot.status_updater_control(slot, False)
 
-    def set_slot(self, slot: int, cb):
-        assert slot < len(self._handlers)
-
+    def set_slot(self, slot: str, cb):
+        slot_idx = self.mcu_updater_slots[slot]
         if callable(cb):
-            if not self._is_enabled[slot]:
-                self._is_enabled[slot] = True
-                self._enable_slot(slot)
-            self._handlers[slot] = cb
+            if not self._is_enabled[slot_idx]:
+                self._is_enabled[slot_idx] = True
+                self._enable_slot(slot_idx)
+            self._handlers[slot_idx] = cb
         else:
-            if self._is_enabled[slot]:
-                self._is_enabled[slot] = False
-                self._disable_slot(slot)
-            self._handlers[slot] = lambda x: None
+            if self._is_enabled[slot_idx]:
+                self._is_enabled[slot_idx] = False
+                self._disable_slot(slot_idx)
+            self._handlers[slot_idx] = lambda x: None
 
     def read(self):
         data = self._robot.status_updater_read()
