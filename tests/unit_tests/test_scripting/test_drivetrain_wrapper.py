f0c115b496e06bb758f1a6c3514e5bdf4d90867a
Dániel Buga <daniel@revolutionrobotics.org>
2020-01-15 17:13:29 +0100

    Fix test

--- tests/unit_tests/test_scripting/test_drivetrain_wrapper.py
+++ tests/unit_tests/test_scripting/test_drivetrain_wrapper.py
@@ -3,7 +3,7 @@ import unittest
 from mock import Mock, call
 
 from revvy.scripting.resource import Resource
-from revvy.scripting.robot_interface import DriveTrainWrapper, MotorConstants
+from revvy.scripting.robot_interface import DriveTrainWrapper, MotorConstants, ResourceWrapper
 
 
 class TestDrivetrainWrapper(unittest.TestCase):
@@ -16,16 +16,16 @@ class TestDrivetrainWrapper(unittest.TestCase):
         mock_script = Mock()
         mock_script.is_stop_requested = False
 
-        res = Resource()
+        res = ResourceWrapper(Resource(), 1)
 
         dtw = DriveTrainWrapper(mock_script, mock_drivetrain, res)
 
         dtw.drive(MotorConstants.DIRECTION_FWD, 3, MotorConstants.UNIT_SEC, 150, MotorConstants.UNIT_SPEED_RPM)
 
-        self.assertEqual(2, mock_drivetrain.set_speeds.call_count)  # start and stop
+        self.assertEqual(1, mock_drivetrain.set_speeds.call_count)
+        self.assertEqual(1, mock_drivetrain.stop_release.call_count)
 
         self.assertEqual(call(900, 900), mock_drivetrain.set_speeds.call_args_list[0])
-        self.assertEqual(call(0, 0), mock_drivetrain.set_speeds.call_args_list[1])
 
     def test_drive_for_time_calls_set_speeds_pwr(self):
         mock_drivetrain = Mock()
@@ -36,16 +36,16 @@ class TestDrivetrainWrapper(unittest.TestCase):
         mock_script = Mock()
         mock_script.is_stop_requested = False
 
-        res = Resource()
+        res = ResourceWrapper(Resource(), 1)
 
         dtw = DriveTrainWrapper(mock_script, mock_drivetrain, res)
 
         dtw.drive(MotorConstants.DIRECTION_FWD, 3, MotorConstants.UNIT_SEC, 75, MotorConstants.UNIT_SPEED_PWR)
 
-        self.assertEqual(2, mock_drivetrain.set_speeds.call_count)  # start and stop
+        self.assertEqual(1, mock_drivetrain.set_speeds.call_count)
+        self.assertEqual(1, mock_drivetrain.stop_release.call_count)
 
         self.assertEqual(call(900, 900, power_limit=75), mock_drivetrain.set_speeds.call_args_list[0])
-        self.assertEqual(call(0, 0), mock_drivetrain.set_speeds.call_args_list[1])
 
     def test_drive_for_rotation_calls_set_position_rpm(self):
         mock_drivetrain = Mock()
@@ -57,7 +57,7 @@ class TestDrivetrainWrapper(unittest.TestCase):
         mock_script = Mock()
         mock_script.is_stop_requested = False
 
-        res = Resource()
+        res = ResourceWrapper(Resource(), 1)
 
         dtw = DriveTrainWrapper(mock_script, mock_drivetrain, res)
 
@@ -77,7 +77,7 @@ class TestDrivetrainWrapper(unittest.TestCase):
         mock_script = Mock()
         mock_script.is_stop_requested = False
 
-        res = Resource()
+        res = ResourceWrapper(Resource(), 1)
 
         dtw = DriveTrainWrapper(mock_script, mock_drivetrain, res)
 
