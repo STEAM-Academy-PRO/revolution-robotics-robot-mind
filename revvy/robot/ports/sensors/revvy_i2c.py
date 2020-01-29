99c0e9e3f349b0445980dc9bea9f48f973bb8e22
Dániel Buga <daniel@revolutionrobotics.org>
2020-01-29 19:15:53 +0100

    Make the color sensor a bit more self contained

--- revvy/robot/ports/sensors/revvy_i2c.py
+++ revvy/robot/ports/sensors/revvy_i2c.py
@@ -2,20 +2,10 @@
 import struct
 from enum import Enum
 
+from typing import NamedTuple
+
 from revvy.robot.ports.common import PortInstance
 from revvy.robot.ports.sensors.base import BaseSensorPortDriver
-
-colors = (
-    "None",
-    "Black",
-    "Blue",
-    "Green",
-    "Yellow",
-    "Cyan",
-    "Red",
-    "White",
-    "Purple"
-)
 
 
 class RevvyI2CSensorState(Enum):
@@ -30,34 +20,61 @@ class BaseRevvyI2CSensor(BaseSensorPortDriver):
     WRITE_SENSOR_ADDRESS = 0
     WRITE_SELECT_SENSOR_MODE = 1
 
-    SENSOR_MODE_IDLE = 0
-    SENSOR_MODE_DEMO = 1
-
-    def __init__(self, sensor_address, port: PortInstance):
+    def __init__(self, sensor_address, port: PortInstance, mode):
         super().__init__("RevvyI2C", port)
         assert 0 <= sensor_address <= 255, f"Invalid I2C sensor address {sensor_address}"
+        self._mode = mode
         self._sensor_address = sensor_address
 
     def on_port_type_set(self):
         self._interface.write_sensor_port(self._port.id, (self.WRITE_SENSOR_ADDRESS, self._sensor_address))
 
-    def update_status(self, data):
+    def convert_sensor_value(self, data):
         state = RevvyI2CSensorState(data[0])
 
         if state == RevvyI2CSensorState.STATE_DETECTED_FIRMWARE:
             print(state)
-            self._interface.write_sensor_port(self._port.id, (self.WRITE_SELECT_SENSOR_MODE, self.SENSOR_MODE_DEMO))
+            self._interface.write_sensor_port(self._port.id, (self.WRITE_SELECT_SENSOR_MODE, self._mode))
         elif state == RevvyI2CSensorState.STATE_OPERATIONAL:
-            if len(data) > 2:
-                state, mode, line_pos, color = struct.unpack('<4b', data)
-                print(line_pos, colors[color])
+            return self.convert_sensor_data(data)
         else:
             print(state)
 
-    def convert_sensor_value(self, raw):
-        pass
+    def convert_sensor_data(self, data):
+        raise NotImplementedError
+
+
+class Color(Enum):
+    NONE = 0
+    BLACK = 1
+    BLUE = 2
+    GREEN = 3
+    YELLOW = 4
+    CYAN = 5
+    RED = 6
+    WHITE = 7
+    PURPLE = 8
+
+
+class ColorSensorData(NamedTuple):
+    line_pos: int
+    color: Color
+
+
+null_color = ColorSensorData(0, Color.NONE)
 
 
 def revvy_color_sensor(port: PortInstance, _):
-    sensor = BaseRevvyI2CSensor(0xC0, port)
+    sensor = BaseRevvyI2CSensor(0xC0, port, mode=1)
+
+    def convert_color_sensor_data(data):
+        if len(data) > 2:
+            state, mode, line_pos, color = struct.unpack('<4b', data)
+
+            return ColorSensorData(line_pos, Color(color))
+        else:
+            return null_color
+
+    sensor.convert_sensor_data = convert_color_sensor_data
+
     return sensor
