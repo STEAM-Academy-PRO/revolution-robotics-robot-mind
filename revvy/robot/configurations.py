e04dd1c4dc2515f668132ab536fb52b804d0e608
Dániel Buga <daniel@revolutionrobotics.org>
2020-01-30 16:51:02 +0100

    Remove old motor configurations, update controllers to use new pwm range

--- revvy/robot/configurations.py
+++ revvy/robot/configurations.py
@@ -5,7 +5,7 @@ class Motors:
     RevvyMotor = {
         'driver': 'DcMotor',
         'config': {
-            'speed_controller': [1 / 35, 0.25, 0, -100, 100],
+            'speed_controller': [1 / 18, 0.5, 0, -200, 200],
             'position_controller': [4, 0, 0, -600, 600],
             'acceleration_limits': [14400, 3600],
             'encoder_resolution': 1536
@@ -14,46 +14,10 @@ class Motors:
     RevvyMotor_CCW = {
         'driver': 'DcMotor',
         'config': {
-            'speed_controller': [1 / 35, 0.25, 0, -100, 100],
+            'speed_controller': [1 / 18, 0.5, 0, -200, 200],
             'position_controller': [4, 0, 0, -600, 600],
             'acceleration_limits': [14400, 3600],
             'encoder_resolution': -1536
-        }
-    }
-    RevvyMotor_Old = {
-        'driver': 'DcMotor',
-        'config': {
-            'speed_controller': [1 / 25, 0.3, 0, -100, 100],
-            'position_controller': [10, 0, 0, -900, 900],
-            'acceleration_limits': [14400, 3600],
-            'encoder_resolution': 1168
-        }
-    }
-    RevvyMotor_Old_CCW = {
-        'driver': 'DcMotor',
-        'config': {
-            'speed_controller': [1 / 25, 0.3, 0, -100, 100],
-            'position_controller': [10, 0, 0, -900, 900],
-            'acceleration_limits': [14400, 3600],
-            'encoder_resolution': -1168
-        }
-    }
-    RevvyMotor_Dexter = {
-        'driver': 'DcMotor',
-        'config': {
-            'speed_controller': [1 / 8, 0.3, 0, -100, 100],
-            'position_controller': [10, 0, 0, -900, 900],
-            'acceleration_limits': [14400, 3600],
-            'encoder_resolution': 292
-        }
-    }
-    RevvyMotor_Dexter_CCW = {
-        'driver': 'DcMotor',
-        'config': {
-            'speed_controller': [1 / 8, 0.3, 0, -100, 100],
-            'position_controller': [10, 0, 0, -900, 900],
-            'acceleration_limits': [14400, 3600],
-            'encoder_resolution': -292
         }
     }
 
