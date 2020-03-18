61bff719552205a672dc398954caf2f400ae1722
Dániel Buga <daniel@revolutionrobotics.org>
2020-03-18 14:20:17 +0100

    Add a small deadband to motor linearization

--- revvy/robot/configurations.py
+++ revvy/robot/configurations.py
@@ -12,7 +12,7 @@ class Motors:
             'position_controller': [0.1, 0.0000, 0, -150, 150],
             'acceleration_limits': [500, 500],
             'max_current': 1.5,
-            'linearity': {5.0154: 18, 37.0370: 60, 67.7083: 100, 97.4151: 140, 144.0972: 200},
+            'linearity': {0.5: 0, 5.0154: 18, 37.0370: 60, 67.7083: 100, 97.4151: 140, 144.0972: 200},
             'encoder_resolution': 12,
             'gear_ratio': 64.8
         }
@@ -24,7 +24,7 @@ class Motors:
             'position_controller': [0.1, 0.0000, 0, -150, 150],
             'acceleration_limits': [500, 500],
             'max_current': 1.5,
-            'linearity': {5.0154: 18, 37.0370: 60, 67.7083: 100, 97.4151: 140, 144.0972: 200},
+            'linearity': {0.5: 0, 5.0154: 18, 37.0370: 60, 67.7083: 100, 97.4151: 140, 144.0972: 200},
             'encoder_resolution': -12,
             'gear_ratio': 64.8
         }
