e8fcb8b786bab20bc946b1af4942d5e859a9cf9b
SPapina <papina.sergei@gmail.com>
2022-06-10 14:06:10 +0300

    clear code

--- revvy/robot/ports/sensors/simple.py
+++ revvy/robot/ports/sensors/simple.py
@@ -9,10 +9,10 @@ from revvy.robot.ports.sensors.base import BaseSensorPortDriver
 # noinspection PyUnusedLocal
 def bumper_switch(port: PortInstance, cfg):
     sensor = BaseSensorPortDriver('BumperSwitch', port)
-    print("\n !!!bumper!!!\n")
+    # print("\n !!!bumper!!!\n")
 
     def process_bumper(raw):
-        print("bumper info:", len(raw), raw)
+        # print("bumper info:", len(raw), raw)
         assert len(raw) == 2
         return raw[0] == 1
 
@@ -40,31 +40,10 @@ def hcsr04(port: PortInstance, cfg):
 # noinspection PyUnusedLocal
 def softeq_cs(port: PortInstance, cfg):
     sensor = BaseSensorPortDriver('RGB', port)
-    print("\n !!!color sensor!!!\n")
+    # print("\n !!!color sensor!!!\n")
 
     def process_softeq_cs(raw):
-        # my_hex = raw
-        # print(" ".join(hex(n) for n in my_hex))
-        # assert len(raw) == 37# 8
-        # color_red, color_green, color_blue, color_c = struct.unpack("<hhhh", raw)
-        # if color_red == 0 and color_green == 0 and color_blue == 0:
-        #     return None
-        # print("0,215,0", rgb_to_hsv(0, 215, 0))
-        # he = struct.iter_unpack("<BBB", my_hex)
-
-        # for _ in struct.iter_unpack("<BBB", raw):
-        #     (r, g, b) = _
-        #     print("hhh ", r, g, b, *_, rgb_to_hsv(*_))
-
-        # li = [rgb_to_hsv(*_) for _ in struct.iter_unpack("<BBB", raw)]
-        # back = round(li[0][0], 1)
-        # right = round(li[1][0], 1)
-        # left = round(li[2][0], 1)
-        # forward = round(li[3][0], 1)
-        # print(back, right, left, forward)
-
-        # ColorRGB(color_red, color_green, color_blue, color_c)
-        return raw  # bytes("None1234567890None", "UTF-8")  # ColorRGB(color_red, color_green, color_blue, color_c)
+        return raw
 
     sensor.convert_sensor_value = process_softeq_cs
     return sensor
