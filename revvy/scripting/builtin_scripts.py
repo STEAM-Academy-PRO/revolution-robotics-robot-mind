ee1e54616d01597b9724d1aac8377165ecc3acf7
SPapina <papina.sergei@gmail.com>
2022-06-16 17:04:46 +0300

    works, still use just H

--- revvy/scripting/builtin_scripts.py
+++ revvy/scripting/builtin_scripts.py
@@ -59,15 +59,26 @@ class ColorHSV:
         return self.value
 
 
+def detect_line_background_colors(fd, ct, lt, rt):
+    line = 1
+    background = 1
+    if abs(1 - fd/ct) < 0.2 and abs(1 - lt/rt) < 0.2:
+        line = (fd+ct)/2
+        background = (lt+rt)/2
+        return line, background
+    return line, background
+
+
 def drive_color(drivetrain_control: DriveTrainWrapper, robot_sensors: SensorPortWrapper):
     """ need to set correct line and background colors """
-    base_color = 20.3   # 14.5
-    background_color_c = 60  # 61.0
-    background_color_f = 61.7  # 59.0
-    background_color_l = 58.4  # 58.0
-    background_color_r = 60  # 56.1
-    delta_base_background = 40
-    base_speed = 0.35  # 0.33  # 0.3
+    base_color = 20.3  # 14.5
+    background_color = 50
+    # background_color_c = 60  # 61.0
+    # background_color_f = 61.7  # 59.0
+    # background_color_l = 58.4  # 58.0
+    # background_color_r = 60  # 56.1
+    # delta_base_background = 40
+    base_speed = 0.2  # 0.33  # 0.3
     count = 0
 
     k_speed = 1 - 0.33 * base_speed
@@ -80,16 +91,31 @@ def drive_color(drivetrain_control: DriveTrainWrapper, robot_sensors: SensorPort
         # get colors
         res = robot_sensors["RGB"].read()
         sensors = [rgb_to_hsv(*_) for _ in struct.iter_unpack("<BBB", res)]
-        # print(sensors)
+        print(sensors)
         forward = sensors[0][0]
         left = 0.76 * sensors[1][0]  # each sensor gives some difference, need to correct
         right = sensors[2][0]
         center = sensors[3][0]
-        print(forward, left, right, center)
+        color_max = max(forward, left, right, center)
+        color_min = min(forward, left, right, center)
+        if color_max > background_color:
+            background_color = color_max
+        if color_min < base_color:
+            base_color = color_min
+        delta_base_background = background_color - base_color
+
+        line, background = detect_line_background_colors(forward, center, left, right)
+        print(line, background)
+        # print(detect_line_background_colors(forward, left, right, center))
+        print(forward, left, right, center, base_color, background_color)
 
         # check: stop when line loosed and exit from function
-        if abs(background_color_f-forward) < 7 and abs(background_color_c-center) < 7 \
-                and abs(background_color_r-right) < 7 and abs(background_color_l-left) < 7:
+        # if abs(background_color-forward) < 7 and abs(background_color-center) < 7 \
+        #         and abs(background_color-right) < 7 and abs(background_color-left) < 7:
+        if not (abs(base_color - forward) < 7 or abs(base_color - center) < 7
+                or abs(base_color - right) < 7 or abs(base_color - left) < 7):
+            # print("")
+            # else:
             drivetrain_control.set_speeds(
                 map_values(0, 0, 1, 0, 120),
                 map_values(0, 0, 1, 0, 120))
@@ -98,19 +124,20 @@ def drive_color(drivetrain_control: DriveTrainWrapper, robot_sensors: SensorPort
 
         sl = base_speed
         sr = base_speed
-        delta_lr = abs(right-left)
-        delta = delta_base_background*1.03
+        delta_lr = abs(right - left)
+        delta = delta_base_background * 1.03
 
-        k_diff = (1-k_speed) \
-                + k_speed*((k_angle*sqrt(delta-delta_lr)+(delta_base_background-k_angle*sqrt(delta))) / delta)
+        k_diff = (1 - k_speed) \
+                 + k_speed * ((k_angle * sqrt(delta - delta_lr) + (
+                    delta_base_background - k_angle * sqrt(delta))) / delta)
         print("   k_diff", k_diff, "\n   k_speed", k_speed)
         speed_primary = base_speed * k_diff
         speed_secondary = base_speed / k_diff
 
         """ red line (17), light background (70)
             if forward sensor at line """
-        if abs(base_color-forward) < 55:
-            compare = delta_base_background/20
+        if abs(base_color - forward) < 200:  # 55:
+            compare = delta_base_background / 20
             if delta_lr > compare:
                 sr = speed_primary
                 sl = speed_secondary
