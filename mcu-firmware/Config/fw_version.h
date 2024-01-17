89013b2f0dde3e0bb784de0019fbe4cd22c3eb42
Dániel Buga <bugadani@gmail.com>
2024-01-17 12:58:04 +0100

    Update mcu-firmware, fix firmware update issues

--- mcu-firmware/Config/fw_version.h
+++ mcu-firmware/Config/fw_version.h
@@ -1,6 +1,6 @@
 
 /* This file is generated during the build process */
 #ifndef FW_VERSION
-#define FW_VERSION "1230"
-#define FW_VERSION_NUMBER 1230
+#define FW_VERSION "1242-main"
+#define FW_VERSION_NUMBER 1242
 #endif
