b7c61119430ee4a94aec3fb89e5ed6a9c0a370be
Mr. Robot <mrrobot@asandia.com>
2024-02-07 10:25:32 +0100

    MORE unit tests.

--- pi-firmware/revvy/utils/bit_packer.py
+++ pi-firmware/revvy/utils/bit_packer.py
@@ -4,21 +4,21 @@ import struct
 
 def pack_2_bit_number_array_32(numbers: [int]) -> bytearray:
     """
-        returns 8 bytes
-        converts:  1 2 0 1 => 01100001 => b\x61
+        returns 8 bytes, reversed bit order!
+        converts:  1 2 0 1 => REVERSE: 01 10 00 01 => b\x
         pads the last bytes with zeroes.
     """
     packed_value = 0
 
     if len(numbers) > 32:
-        raise ValueError("Only max 16 numbers are allowed.")
+        raise ValueError("Only max 32 numbers are allowed.")
 
     for i, num in enumerate(numbers):
         if not 0 <= num < 4:
             raise ValueError("Each number must be a 2-bit number (0, 1, 2, or 3)")
         packed_value |= (num << (2 * i))
 
-    return struct.pack("<Q", packed_value)
+    return struct.pack(">Q", packed_value)
 
 
 
@@ -26,7 +26,8 @@ def unpack_2_bit_number_array_32(byte_array: bytearray) -> [int]:
     """ Unpacks 32 element int [0-3] array from a 8 sized bytearray """
 
     result = []
-    for byte in byte_array:
+    # Reverse byte order.
+    for byte in byte_array[::-1]:
         # Extract each 2-bit number from the byte
         for i in range(4):
             two_bit_number = (byte >> (2 * i)) & 0b11
