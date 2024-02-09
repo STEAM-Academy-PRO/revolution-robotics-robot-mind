27fc7acc034b81af922417890c54191d3be6631c
Gusztav <symunona+github@gmail.com>
2024-02-09 14:01:57 +0100

    Update pi-firmware/revvy/robot/imu.py
    Co-authored-by: Dániel Buga <bugadani@gmail.com>

--- pi-firmware/revvy/robot/imu.py
+++ pi-firmware/revvy/robot/imu.py
@@ -53,7 +53,7 @@ class IMU:
         # print('update_axl_data', data, self._acceleration)
 
     def update_gyro_data(self, data):
-        """ LIS3MDL sensor configuration constants """
+        """ LSM6DS3H sensor configuration constants """
         self._rotation = self._read_vector(data, 0.035*1.03)
         # print('update_gyro_data', data, self._rotation)
 
