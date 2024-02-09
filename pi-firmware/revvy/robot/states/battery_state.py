daaaf9b24a954469ba1d4e6fd0fb7dad0e343c54
Mr. Robot <mrrobot@asandia.com>
2024-02-09 14:14:45 +0100

    Added comment.

--- pi-firmware/revvy/robot/states/battery_state.py
+++ pi-firmware/revvy/robot/states/battery_state.py
@@ -9,15 +9,17 @@ class BatteryState(Observable):
     def __init__(self, throttle_interval):
         super().__init__(throttle_interval)
         # Battery gets checked every 5ms and it reads out a DAC value, which has plenty of noise.
-        # To smoothen that out, here is a 10000 sized min calculator, meaning: takes every 5ms measurement,
+        # To smoothen that out, here is a 500 sized min calculator (~2.5sec), meaning: takes every 5ms measurement,
         # puts it in an array, gets the min value, rounds it to disable flickering between values.
-        self._main = SmoothingObservable(0, window_size=10000, throttle_interval=5,
+        self._main = SmoothingObservable(0, window_size=500, throttle_interval=5,
                                          smoothening_function=lambda history: round(min(history)))
         self._charger_status = Observable(0, throttle_interval=2)
         self._motor = SmoothingObservable(0, window_size=50, throttle_interval=1)
 
-        # If I understood it correctly, we can not really detect this, only
-        # the motor voltage. Why are we having this then???
+        # The MCU is supposed to send indication. It has code that detect batteries under 4V as not plugged in.
+        # This is a reasonable assumption, the minimum battery pack voltage (0%) is set to 5.4V.
+        # We just can't say if the pack is really not plugged in vs not all 6 batteries are inserted vs
+        # the batteries are just reeeeally flat.
         self._motor_battery_present = Observable(0, throttle_interval=2)
 
     def set(self, new_data: BatteryStatus):
