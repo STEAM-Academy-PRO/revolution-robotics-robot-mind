59e9e5565eda3871a7cb3131251c8511b06c9dad
Dániel Buga <bugadani@gmail.com>
2024-02-16 13:59:21 +0100

    Clean up in IMU

--- pi-firmware/revvy/robot/imu.py
+++ pi-firmware/revvy/robot/imu.py
@@ -1,10 +1,32 @@
 """ Inertial Measurement Unit: gyro and accelerometer """
 
-import collections
 import struct
+from typing import NamedTuple
 
-Vector3D = collections.namedtuple("Vector3D", ["x", "y", "z"])
-Orientation3D = collections.namedtuple("Orientation3D", ["pitch", "roll", "yaw"])
+
+class Vector3D(NamedTuple):
+    x: float
+    y: float
+    z: float
+
+    @staticmethod
+    def deserialize(data):
+        return Vector3D(*struct.unpack("<hhh", data))
+
+    def __mul__(self, value) -> "Vector3D":
+        return Vector3D(self.x * value, self.y * value, self.z * value)
+
+    __rmul__ = __mul__
+
+
+class Orientation3D(NamedTuple):
+    pitch: float
+    roll: float
+    yaw: float
+
+    @staticmethod
+    def deserialize(data):
+        return Orientation3D(*struct.unpack("<fff", data))
 
 
 class IMU:
@@ -16,45 +38,39 @@ class IMU:
         self._relative_yaw_angle = 0
 
     @property
-    def yaw_angle(self):
+    def yaw_angle(self) -> float:
         return self._yaw_angle
 
     @property
-    def relative_yaw_angle(self):
+    def relative_yaw_angle(self) -> float:
         return self._relative_yaw_angle  # TODO pinning is not yet implemented
 
     @property
-    def acceleration(self):
+    def acceleration(self) -> Vector3D:
         return self._acceleration
 
     @property
-    def rotation(self):
+    def rotation(self) -> Vector3D:
         return self._rotation
 
     @property
-    def orientation(self):
+    def orientation(self) -> Orientation3D:
         return self._orientation
-
-    @staticmethod
-    def _read_vector(data, lsb_value):
-        (x, y, z) = struct.unpack("<hhh", data)
-        return Vector3D(x * lsb_value, y * lsb_value, z * lsb_value)
 
     def update_yaw_angles(self, data):
         (self._yaw_angle, self._relative_yaw_angle) = struct.unpack("<ll", data)
         # print('update_yaw_angles', data)
 
     def update_axl_data(self, data):
-        """LSM6DS3H sensor configuration constants"""
-        self._acceleration = self._read_vector(data, 0.061)
+        # LSM6DS3H sensor configuration constants
+        self._acceleration = 0.061 * Vector3D.deserialize(data)
         # print('update_axl_data', data, self._acceleration)
 
     def update_gyro_data(self, data):
-        """LSM6DS3H sensor configuration constants"""
-        self._rotation = self._read_vector(data, 0.035 * 1.03)
+        # LSM6DS3H sensor configuration constants
+        self._rotation = 0.035 * 1.03 * Vector3D.deserialize(data)
         # print('update_gyro_data', data, self._rotation)
 
     def update_orientation_data(self, data):
-        values = struct.unpack("<fff", data)
-        self._orientation = Orientation3D(*values)
+        self._orientation = Orientation3D.deserialize(data)
         # print('update_orientation_data', values)
