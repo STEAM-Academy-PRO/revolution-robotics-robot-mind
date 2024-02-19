fd11335ed6a930a2c5cae9046070e1b607d2adf6
Dániel Buga <bugadani@gmail.com>
2024-02-19 11:30:53 +0100

    Fix known exceptions (#165)
    * Fix missed driver delegation
    
    * Resolve missing clear fn

--- pi-firmware/revvy/scripting/robot_interface.py
+++ pi-firmware/revvy/scripting/robot_interface.py
@@ -318,10 +318,10 @@ class MotorPortWrapper(Wrapper):
         self.using_resource(set_speed_fns[unit_rotation][direction])
 
     def stop(self, action):
-        self.using_resource(partial(self._motor.stop, action))
+        self.using_resource(partial(self._motor.driver.stop, action))
 
 
-def wrap_async_method(owner, method):
+def wrap_async_method(owner: Wrapper, method):
     def _wrapper(*args, **kwargs):
         def _interrupted():
             if awaiter:
