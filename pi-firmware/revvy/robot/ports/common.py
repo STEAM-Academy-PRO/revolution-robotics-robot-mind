b0640452349b24f2e99d9c26331dd37382a41dbb
Bendeguz Korcsog <bendeguz.korcsog@steamacademy.pro>
2024-02-29 13:21:10 +0100

    fix(log): log scoping does not work if we are using base classes

--- pi-firmware/revvy/robot/ports/common.py
+++ pi-firmware/revvy/robot/ports/common.py
@@ -28,7 +28,7 @@ class PortDriver(ABC):
         self._driver_name = driver_name
         self._port = port
         self._on_status_changed = SimpleEventEmitter()
-        self.log = get_logger(driver_name, base=port.log)
+        self.log = get_logger(driver_name)
 
     @property
     def driver_name(self) -> str:
