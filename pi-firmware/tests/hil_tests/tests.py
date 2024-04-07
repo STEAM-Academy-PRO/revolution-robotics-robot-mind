d99a1b451737180c1f58afef59f983d6500d1376
Dániel Buga <bugadani@gmail.com>
2024-04-07 18:53:12 +0200

    Add a test case that drives the real motor driver

--- pi-firmware/tests/hil_tests/tests.py
+++ pi-firmware/tests/hil_tests/tests.py
@@ -138,7 +138,7 @@ robot.motors["motor4"].pos
 def motors_dont_cause_errors(log: Logger, controller: ProgrammedRobotController):
     """
     A test case that configures a script to drive the robot and motors. Since no motors and
-    motor battery is connected, the blocks should essentially be no-op. However, this test
+    motor battery are connected, the blocks should essentially be no-op. However, this test
     still verifies that motor commands reach the MCU and don't cause errors.
     """
 
@@ -217,6 +217,85 @@ for motor2 in robot.motors:
   motor2.stop(action=Motor.ACTION_RELEASE)
 
 robot.motors["motor4"].pos = 0
+"""
+            ),
+        },
+        0,
+    )
+
+    controller.configure(config)
+
+    log("Start script")
+    controller.press_button(1)
+
+    controller.wait_for_scripts_to_end()
+
+
+def missing_motor_does_not_block_script(log: Logger, controller: ProgrammedRobotController):
+    """
+    A test case that configures a script to drive a motors. Since no motors and
+    motor battery are connected, the motor should quickly report a BLOCKED status and the script
+    should move on.
+    """
+
+    def fail_on_script_error(*e) -> None:
+        # In this test, if we encounter an error, let's just stop and exit
+        controller.robot_manager.exit(RevvyStatusCode.ERROR)
+
+    controller.robot_manager.on(RobotEvent.ERROR, fail_on_script_error)
+    controller.with_timeout(20.0)
+
+    config = RobotConfig()
+    # intentionally not EmulatedRevvyMotor to test blocking behaviour
+    config.add_motor(Motors.RevvyMotor_CCW, "motor1")
+    config.add_motor(None, "motor2")
+    config.add_motor(None, "motor3")
+    config.add_motor(None, "motor4")
+    config.add_motor(None, "motor5")
+    config.add_motor(None, "motor6")
+
+    config.drivetrain.left.append(1)
+
+    config.process_script(
+        {
+            "assignments": {"buttons": [{"id": 1, "priority": 0}]},
+            "pythonCode": b64_encode_str(
+                """
+#
+# drive blockly options
+#
+robot.drivetrain.set_speed(direction=Motor.DIRECTION_FWD, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.drivetrain.set_speed(direction=Motor.DIRECTION_FWD, speed=75, unit_speed=Motor.UNIT_SPEED_PWR)
+robot.drivetrain.set_speed(direction=Motor.DIRECTION_BACK, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.drivetrain.set_speed(direction=Motor.DIRECTION_BACK, speed=75, unit_speed=Motor.UNIT_SPEED_PWR)
+
+robot.drive(direction=Motor.DIRECTION_FWD, rotation=0.1, unit_rotation=Motor.UNIT_SEC, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.drive(direction=Motor.DIRECTION_FWD, rotation=0.1, unit_rotation=Motor.UNIT_SEC, speed=75, unit_speed=Motor.UNIT_SPEED_PWR)
+robot.drive(direction=Motor.DIRECTION_FWD, rotation=3, unit_rotation=Motor.UNIT_ROT, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.drive(direction=Motor.DIRECTION_FWD, rotation=3, unit_rotation=Motor.UNIT_ROT, speed=75, unit_speed=Motor.UNIT_SPEED_PWR)
+robot.drive(direction=Motor.DIRECTION_BACK, rotation=0.1, unit_rotation=Motor.UNIT_SEC, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.drive(direction=Motor.DIRECTION_BACK, rotation=0.1, unit_rotation=Motor.UNIT_SEC, speed=75, unit_speed=Motor.UNIT_SPEED_PWR)
+robot.drive(direction=Motor.DIRECTION_BACK, rotation=3, unit_rotation=Motor.UNIT_ROT, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.drive(direction=Motor.DIRECTION_BACK, rotation=3, unit_rotation=Motor.UNIT_ROT, speed=75, unit_speed=Motor.UNIT_SPEED_PWR)
+
+# blockly only generates unit_speed=RPM
+# beware that UNIT_TURN_ANGLE turns take 3 seconds to time out
+robot.turn(direction=Motor.DIRECTION_LEFT, rotation=90, unit_rotation=Motor.UNIT_TURN_ANGLE, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.turn(direction=Motor.DIRECTION_LEFT, rotation=0.1, unit_rotation=Motor.UNIT_SEC, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.turn(direction=Motor.DIRECTION_RIGHT, rotation=90, unit_rotation=Motor.UNIT_TURN_ANGLE, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+robot.turn(direction=Motor.DIRECTION_RIGHT, rotation=0.1, unit_rotation=Motor.UNIT_SEC, speed=75, unit_speed=Motor.UNIT_SPEED_RPM)
+#
+# motor blockly options
+#
+robot.motors["motor1"].move(direction=Motor.DIRECTION_FWD, amount=0.1, unit_amount=Motor.UNIT_SEC, limit=75, unit_limit=Motor.UNIT_SPEED_RPM)
+robot.motors["motor1"].move(direction=Motor.DIRECTION_FWD, amount=0.1, unit_amount=Motor.UNIT_SEC, limit=75, unit_limit=Motor.UNIT_SPEED_PWR)
+robot.motors["motor1"].move(direction=Motor.DIRECTION_FWD, amount=3, unit_amount=Motor.UNIT_DEG, limit=75, unit_limit=Motor.UNIT_SPEED_RPM)
+robot.motors["motor1"].move(direction=Motor.DIRECTION_FWD, amount=3, unit_amount=Motor.UNIT_DEG, limit=75, unit_limit=Motor.UNIT_SPEED_PWR)
+robot.motors["motor1"].move(direction=Motor.DIRECTION_FWD, amount=3, unit_amount=Motor.UNIT_ROT, limit=75, unit_limit=Motor.UNIT_SPEED_RPM)
+robot.motors["motor1"].move(direction=Motor.DIRECTION_FWD, amount=3, unit_amount=Motor.UNIT_ROT, limit=75, unit_limit=Motor.UNIT_SPEED_PWR)
+robot.motors["motor1"].spin(direction=Motor.DIRECTION_FWD, rotation=75, unit_rotation=Motor.UNIT_SPEED_RPM)
+robot.motors["motor1"].spin(direction=Motor.DIRECTION_FWD, rotation=75, unit_rotation=Motor.UNIT_SPEED_PWR)
+robot.motors["motor1"].stop(action=Motor.ACTION_RELEASE)
 """
             ),
         },
@@ -443,6 +522,7 @@ if __name__ == "__main__":
             sensors_can_be_read,
             test_motor_for_i2c_bug,
             motors_dont_cause_errors,
+            missing_motor_does_not_block_script,
             trying_to_access_uncofigured_motor_raises_error,
             trying_to_access_uncofigured_sensor_raises_error,
             trying_to_drive_without_drivetrain_motors_is_no_op,
