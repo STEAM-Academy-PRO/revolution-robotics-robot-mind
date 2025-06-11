f130e7d7b5ed825075b98e962147e0a5e7d82636
Mr. Robot <mrrobot@asandia.com>
2025-06-11 13:37:08 +0200

    Transcript error -n

--- robot-rc-websocket/src/views/BlocklyAiView.tsx
+++ robot-rc-websocket/src/views/BlocklyAiView.tsx
@@ -52,7 +52,7 @@ function BlocklyAIView() {
     console.log("AI RC recognition result:", event);
     let fullTranscript = "";
     for (let i = 0; i < event.results.length; i++) {
-      fullTranscript += event.results[i][0].transcript + "n";
+      fullTranscript += event.results[i][0].transcript + "\n";
     }
     setText(fullTranscript);
   };
