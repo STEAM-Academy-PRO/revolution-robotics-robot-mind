3a287c8bdfcf628d5e2f7020790f742b1e2334f9
Mr. Robot <mrrobot@asandia.com>
2025-06-11 13:30:04 +0200

    removed unnecessary trailing.

--- robot-rc-websocket/src/settings.ts
+++ robot-rc-websocket/src/settings.ts
@@ -5,7 +5,7 @@ export const [conn, setConn] = createSignal<SocketWrapper | null>(null)
 export const [endpoint, setEndpoint] = createSignal(localStorage.getItem('endpoint') || 'raspberrypi.local');
 
 // TODO: My locally hosted blockly: http://localhost/robot/blockly/ - make it a package.
-export const [blocklyUrlBase, setBlocklyUrlBase] = createSignal(localStorage.getItem('blockly') || 'blockly/');
+export const [blocklyUrlBase, setBlocklyUrlBase] = createSignal(localStorage.getItem('blockly') || 'blockly');
 
 createEffect(() => {
     localStorage.setItem('endpoint', endpoint());
