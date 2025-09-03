3980cd128d4c8c1bf7545ae67a8ff11cf2998452
Mr. Robot <symunona@gmail.com>
2025-09-03 20:18:35 +0200

    Default address to hosting system.

--- robot-rc-websocket/src/settings.ts
+++ robot-rc-websocket/src/settings.ts
@@ -2,7 +2,7 @@ import { createEffect, createSignal } from "solid-js";
 import { SocketWrapper } from "./utils/Communicator";
 
 export const [conn, setConn] = createSignal<SocketWrapper | null>(null)
-export const [endpoint, setEndpoint] = createSignal(localStorage.getItem('endpoint') || 'raspberrypi.local');
+export const [endpoint, setEndpoint] = createSignal(localStorage.getItem('endpoint') || location.hostname || 'raspberrypi.local');
 
 // TODO: My locally hosted blockly: http://localhost/robot/blockly/ - make it a package.
 export const [blocklyUrlBase, setBlocklyUrlBase] = createSignal(localStorage.getItem('blockly') || 'blockly');
